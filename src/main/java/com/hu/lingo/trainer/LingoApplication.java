f62d9b2536ce3b03d36288c71327ab72fcf16f05
Redouanelh <redouan_school@outlook.com>
2020-12-02 14:53:34 +0100

    Correct way of using env variables for database credentials

--- src/main/java/com/hu/lingo/trainer/LingoApplication.java
+++ src/main/java/com/hu/lingo/trainer/LingoApplication.java
@@ -3,21 +3,11 @@ package com.hu.lingo.trainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Properties;
-
 @SpringBootApplication
 public class LingoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication(LingoApplication.class);
-
-		Properties props = new Properties();
-		props.put("spring.datasource.url", System.getenv("DB_URL"));
-		props.put("spring.datasource.username", System.getenv("DB_USER"));
-		props.put("spring.datasource.password", System.getenv("DB_PASS"));
-		application.setDefaultProperties(props);
-
-		application.run(args);
+		SpringApplication.run(LingoApplication.class, args);
 	}
 
 }
