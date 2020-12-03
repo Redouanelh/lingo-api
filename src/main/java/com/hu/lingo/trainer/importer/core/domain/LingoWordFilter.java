9dbd7f8ce1fcc78554d62b628f6ca9203364f101
Redouanelh <redouan_school@outlook.com>
2020-12-03 13:59:43 +0100

    Added codecomment. Removed words.txt from tests resources. Renamed words.txt to basiswoorden-gekeurd.txt

--- src/main/java/com/hu/lingo/trainer/importer/core/domain/LingoWordFilter.java
+++ src/main/java/com/hu/lingo/trainer/importer/core/domain/LingoWordFilter.java
@@ -4,6 +4,7 @@ public class LingoWordFilter implements WordFilter {
 
     @Override
     public boolean verify(String word) {
+        // only accept lower case letters with words where length >= 5 and <= 7
         return word.matches("^[a-z]{5,7}$");
     }
 
