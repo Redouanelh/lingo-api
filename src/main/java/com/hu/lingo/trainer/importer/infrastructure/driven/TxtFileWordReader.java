0c91e431ab4821c1bdeb138453be65bd5cea5909
Redouanelh <redouan_school@outlook.com>
2020-12-03 17:46:33 +0100

    Created implementation classes for WordReader and WordWriter ports

---
+++ src/main/java/com/hu/lingo/trainer/importer/infrastructure/driven/TxtFileWordReader.java
@@ -0,0 +1,12 @@
+package com.hu.lingo.trainer.importer.infrastructure.driven;
+
+import com.hu.lingo.trainer.importer.core.ports.WordReader;
+
+import java.util.stream.Stream;
+
+public class TxtFileWordReader implements WordReader {
+    @Override
+    public Stream<String> readWords() {
+        return null;
+    }
+}
---
+++ src/main/java/com/hu/lingo/trainer/importer/infrastructure/driven/TxtFileWordWriter.java
@@ -0,0 +1,12 @@
+package com.hu.lingo.trainer.importer.infrastructure.driven;
+
+import com.hu.lingo.trainer.importer.core.ports.WordWriter;
+
+import java.util.List;
+
+public class TxtFileWordWriter implements WordWriter {
+    @Override
+    public void writeWords(List<String> words) {
+
+    }
+}
