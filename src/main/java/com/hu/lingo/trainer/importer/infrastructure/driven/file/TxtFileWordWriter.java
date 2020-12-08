41c8184825819efe8f1a2e0a13d1a6891ea42d00
Redouanelh <redouan_school@outlook.com>
2020-12-08 16:25:58 +0100

    Working TxtFileWordReader and TxtFileWordWriter.

--- src/main/java/com/hu/lingo/trainer/importer/infrastructure/driven/file/TxtFileWordWriter.java
+++ src/main/java/com/hu/lingo/trainer/importer/infrastructure/driven/file/TxtFileWordWriter.java
@@ -1,14 +1,28 @@
 package com.hu.lingo.trainer.importer.infrastructure.driven.file;
 
 import com.hu.lingo.trainer.importer.core.ports.WordWriter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 @Service
 public class TxtFileWordWriter implements WordWriter {
+    private final Path target;
+
+    public TxtFileWordWriter(@Value("${lingo.target}") Path target) {
+        this.target = target;
+    }
+
     @Override
     public void writeWords(List<String> words) {
-
+        try {
+            Files.writeString(this.target, String.join("\n", words));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
