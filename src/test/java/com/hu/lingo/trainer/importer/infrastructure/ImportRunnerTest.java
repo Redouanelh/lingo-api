785ea2a655ee6b6d5e77ca99965cb50491e25362
Redouanelh <redouan_school@outlook.com>
2020-12-09 12:26:41 +0100

    100% codecoverage for importrunner

--- src/test/java/com/hu/lingo/trainer/importer/infrastructure/ImportRunnerTest.java
+++ src/test/java/com/hu/lingo/trainer/importer/infrastructure/ImportRunnerTest.java
@@ -1,5 +1,34 @@
 package com.hu.lingo.trainer.importer.infrastructure;
 
+import com.hu.lingo.trainer.importer.core.application.WordImporter;
+import com.hu.lingo.trainer.importer.core.domain.WordFilter;
+import com.hu.lingo.trainer.importer.core.ports.WordReader;
+import com.hu.lingo.trainer.importer.core.ports.WordWriter;
+import com.hu.lingo.trainer.importer.infrastructure.driver.cli.ImportRunner;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class ImportRunnerTest {
 
+    @Test
+    void import_words_once_at_startup() {
+
+        WordReader mockReader = mock(WordReader.class);
+        WordFilter mockFilter = mock(WordFilter.class);
+        WordWriter mockWriter = mock(WordWriter.class);
+
+        WordImporter mockWordImporter = mock(WordImporter.class, withSettings().useConstructor(mockReader, mockFilter, mockWriter));
+        ImportRunner mockImportRunner = mock(ImportRunner.class, withSettings().useConstructor(mockWordImporter));
+        mockImportRunner.run();
+
+        verify(mockImportRunner, times(1))
+                .run();
+    }
 }
