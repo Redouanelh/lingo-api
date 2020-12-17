7cecb05e62e5b854feadf7815f84d74f9f23509e
Redouanelh <redouan_school@outlook.com>
2020-12-17 14:13:46 +0100

    Game object

--- src/main/java/com/hu/lingo/trainer/domain/entity/Game.java
+++ src/main/java/com/hu/lingo/trainer/domain/entity/Game.java
@@ -4,9 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.sql.Date;
 
 @Getter
 @Setter
@@ -15,4 +16,29 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "game")
 public class Game extends BaseEntity {
+
+    @Column(name = "round")
+    private int gameRound;
+
+    @Column(name = "score")
+    private int gameScore;
+
+    @Enumerated(EnumType.STRING)
+    private GameStatus gameStatus = GameStatus.ACTIVE;
+
+    @CreationTimestamp
+    private Date created_at;
+
+    @OneToOne()
+    @JoinColumn(name = "player_fk")
+    private Player player;
+
+    public void correctWord() {
+        this.gameScore += 1;
+    }
+
+    public void endGame() {
+        this.gameStatus = GameStatus.FINISHED;
+    }
+
 }
